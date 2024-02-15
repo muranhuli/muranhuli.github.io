641928c52e5b2d24344d48638b9100536595a17a
Luffy Liu <38523623+muranhuli@users.noreply.github.com>
2024-02-15 13:43:55 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   return defineConfig({
-    base: loadEnv(mode, process.cwd()).VITE_BASE_URL,
+    // base: loadEnv(mode, process.cwd()).VITE_BASE_URL,
+    base: './',
     plugins: [vue()]
   })
 }
